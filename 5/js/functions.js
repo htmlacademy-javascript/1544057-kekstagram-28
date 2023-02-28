5528d31fe8ec59136bc0f682458f5fc55110364a
Keks <keks@htmlacademy.ru>
2023-02-28 23:04:23 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/functions.js
@@ -0,0 +1,64 @@
+const validateStringLength = (str, maxLength) => str.length <= maxLength;
+
+// Cтрока короче 20 символов
+validateStringLength('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+validateStringLength('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+validateStringLength('проверяемая строка', 10); // false
+
+const isPalindrome = (str) => {
+  str = str.replace(/\s/g, '').toLowerCase();
+  return str === str.split('').reverse().join('');
+};
+
+// Строка является палиндромом
+isPalindrome('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+isPalindrome('ДовОд'); // true
+// Это не палиндром
+isPalindrome('Кекс'); // false
+// Это палиндром
+isPalindrome('Лёша на полке клопа нашёл '); // true
+
+const extractNumbers = (str) => {
+  const numStr = String(str).match(/\d+/g)?.join('');
+  return numStr ? parseInt(numStr, 10) : NaN;
+};
+
+extractNumbers('2023 год'); // 2023
+extractNumbers('ECMAScript 2022'); // 2022
+extractNumbers('1 кефир, 0.5 батона'); // 105
+extractNumbers('агент 007'); // 7
+extractNumbers('а я томат'); // NaN
+
+extractNumbers(2023); // 2023
+extractNumbers(-1); // 1
+extractNumbers(1.5); // 15
+
+const padString = (str, minLength, padChars) => {
+  if (str.length >= minLength) {
+    return str;
+  }
+
+  const padLength = Math.ceil((minLength - str.length) / padChars.length);
+  const padding = padChars.repeat(padLength).substr(0, minLength - str.length);
+
+  return padding + str;
+};
+
+
+// Добавочный символ использован один раз
+padString('1', 2, '0'); // '01'
+
+// Добавочный символ использован три раза
+padString('1', 4, '0'); // '0001'
+
+// Добавочные символы обрезаны с конца
+padString('q', 4, 'werty'); // 'werq'
+
+// Добавочные символы использованы полтора раза
+padString('q', 4, 'we'); // 'wweq'
+
+// Добавочные символы не использованы, исходная строка не изменена
+padString('qwerty', 4, '0'); // 'qwerty'
