0babbb45b0d22df0193d85a24af4bd16880ecde1
Original-pokemon <eremkin-02@mail.ru>
2023-03-03 14:43:22 +0300

    refactor: move all var in obj param and use decomposition

--- js/photos.mjs
+++ js/photos.mjs
@@ -1,15 +1,18 @@
-const createPhoto = (comments, likes, idGenerator) => {
-  const generatePhotoId = idGenerator;
+const createPhoto = (param) => {
+  const { MIN_PHOTO_ID, MAX_PHOTO_ID, MIN_LIKES, MAX_LIKES } = param.consts;
+  const { getRandomInt, getRandomIntGenerator } = param.func;
+  const generatePhotoId = getRandomIntGenerator(MIN_PHOTO_ID, MAX_PHOTO_ID);
   const id = generatePhotoId();
+
   return {
     id: id,
     url: `photos/${id}.jpg`,
     description: `Описание фотографии ${id}`,
-    likes,
-    comments
+    likes: getRandomInt(MIN_LIKES, MAX_LIKES),
+    comments: param.createComments(param)
   };
 };
 
-const createPhotos = (param) => Array.from({ length: param.count }, () => createPhoto(param.comments, param.likes, param.idGenerator));
+const createPhotos = (param) => Array.from({ length: param.consts.SIMILAR_PHOTO_COUNT }, () => createPhoto(param));
 
 export { createPhotos };
